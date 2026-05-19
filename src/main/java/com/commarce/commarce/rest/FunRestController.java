e02611a0d15a7acf98cb17f0a467747246566190
sadbinsiddique <21-46005-2@student.aiub.edu>
2026-05-19 18:30:34 +0600

    simple controller

---
+++ src/main/java/com/commarce/commarce/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.commarce.commarce.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String Home() {
+        return "hello World";
+    }
+}
