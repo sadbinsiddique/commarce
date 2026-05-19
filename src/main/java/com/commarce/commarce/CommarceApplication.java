aaee8bc3deadb4b65bf5f5b2763809d8ee053cec
sadbinsiddique <21-46005-2@student.aiub.edu>
2026-05-19 15:59:36 +0600

    Initial commit

---
+++ src/main/java/com/commarce/commarce/CommarceApplication.java
@@ -0,0 +1,13 @@
+package com.commarce.commarce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommarceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommarceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commarce/commarce/CommarceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commarce.commarce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommarceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
