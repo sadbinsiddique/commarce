ef806260454b58604f41d45c31c100f4b2421e27
sadbinsiddique <21-46005-2@student.aiub.edu>
2026-05-21 17:17:39 +0600

    Component Scanning

--- src/main/java/com/commarce/commarce/rest/FunRestController.java
+++ src/main/java/com/commarce/commarce/rest/FunRestController.java
@@ -1,9 +1,9 @@
 package com.commarce.commarce.rest;
 
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import util.Coach;
 
 @RestController
 public class FunRestController {
@@ -12,4 +12,19 @@ public class FunRestController {
     public String Home() {
         return "hello World";
     }
+
+    // define a private field for dependency injection
+    private final Coach myCoach;
+
+    @Autowired
+    // constructor injection: Spring will provide a Coach implementation
+    public FunRestController(Coach coach) {
+        this.myCoach = coach;
+    }
+
+    @GetMapping("/daily-workout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
 }
