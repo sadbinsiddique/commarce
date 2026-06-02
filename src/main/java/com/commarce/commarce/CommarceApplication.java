46971ea041047dea6f16985fbde37852191e13e0
sadbinsiddique <mdsiyam377@gmail.com>
2026-06-02 16:30:06 +0600

    modified entry point for custom code config

--- src/main/java/com/commarce/commarce/CommarceApplication.java
+++ src/main/java/com/commarce/commarce/CommarceApplication.java
@@ -1,15 +1,21 @@
 package com.commarce.commarce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication(
-		scanBasePackages = {
-				"com.commarce.commarce",
-				"util"})
-public class CommarceApplication {
+		scanBasePackages = {"com.commarce.commarce", "util"})
+	public class CommarceApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run(CommarceApplication.class, args);
+	}
+
+	// This is Custom Code
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return ignoredArgs -> System.out.println("Hello, World!");
 	}
 
 }
